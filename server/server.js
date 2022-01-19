9b7469a9461068e6c96d62a073dbe782443488c2
Mohammad Ibrahim <hamoodyhmood98@gmail.com>
2022-01-19 12:40:26 +0200

    test commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require(`path`)
 const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname, `..`)
+const port = process.env.PORT || 9000
 // we use this to register some our midlle where and it means somthink that runs in each request that is mean
 // it will run a block of code in each request 
 
@@ -15,6 +16,6 @@ app.get(`*`,(req, res) =>{
     res.sendFile(path.join(publicPath, `index.html`))
 } )
 
-app.listen(9000, () => {
+app.listen(port, () => {
     console.log(`the server is up `);
 })// (9000)is the port that good for all the operating systems 
